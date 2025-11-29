e2a90f25af4f2e4594acd1afed08848dbf2412dc
Guerber-Lilian <lilianguerber@gmail.com>
2025-11-30 00:55:16 +0100

    ajout dossier UserData

---
+++ UserData/Lilian1.inc.php
@@ -0,0 +1,8 @@
+<?php $session = array (
+  'login' => 'Lilian1',
+  'motDePasse' => '$2y$10$dQJu6KbUYAZxVhyWbOFcvOs95kYwcjCS4BUSqCAykjiKh4C4iu5Fm',
+  'prenom' => NULL,
+  'nom' => NULL,
+  'sexe' => NULL,
+  'naissance' => NULL,
+);?>
