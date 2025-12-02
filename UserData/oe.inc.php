5d19db83017a51af306a1b6cb7734851d4d7d399
Jean-Ernest_Wambo <jean-ernest.wambo4@etu.univ-lorraine.fr>
2025-12-02 20:38:05 +0100

    removed the lined links

--- UserData/oe.inc.php
+++ UserData/oe.inc.php
@@ -1,8 +1,8 @@
 <?php $session = array (
   'login' => 'oe',
   'motDePasse' => '$2y$10$dNwal107h5l.TTI.erCcrOsWQn1unxynyKhgTOPkUHhmGCY8k24PK',
-  'prenom' => NULL,
+  'prenom' => 'lilian',
   'nom' => 'paul',
-  'sexe' => 'h',
-  'naissance' => '2004-06-08',
+  'sexe' => 'f',
+  'naissance' => '2004-01-06',
 );?>
